3c525b35abd149d8a37bc5055675814740a5d0f3
Chaitu785 <77534070+Chaitu785@users.noreply.github.com>
2023-02-11 22:05:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Product from "./pages/Product";
+import Product from "./pages/Product"
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
